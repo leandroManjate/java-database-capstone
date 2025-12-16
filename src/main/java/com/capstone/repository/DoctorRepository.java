9140633ff4f8f02518106e1b472ebb974b5c05d9
LEANDRO TITO MANJATE <64547066+leandroManjate@users.noreply.github.com>
2025-12-16 23:00:12 +0200

    Add DoctorRepository interface for Doctor entity

---
+++ src/main/java/com/capstone/repository/DoctorRepository.java
@@ -0,0 +1,10 @@
+package com.capstone.repository;
+
+import com.capstone.model.Doctor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+    Optional<Doctor> findByEmail(String email);
+}
