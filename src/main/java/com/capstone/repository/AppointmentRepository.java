f4b758a27e0cea584f5516ddae0bcc354614502f
LEANDRO TITO MANJATE <64547066+leandroManjate@users.noreply.github.com>
2025-12-16 23:00:58 +0200

    Add DoctorService

---
+++ src/main/java/com/capstone/repository/AppointmentRepository.java
@@ -0,0 +1,11 @@
+package com.capstone.repository;
+
+import com.capstone.model.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    List<Appointment> findByDoctorIdAndAppointmentTimeBetween(Long doctorId, LocalDateTime start, LocalDateTime end);
+}
